8829640802c5f9a093fdaa217a5644be85b5b0f2
medude <medude@github.com>
2018-04-09 11:18:05 -0400

    add stuff

--- face_recognizer.py
+++ face_recognizer.py
@@ -1,37 +1,33 @@
-import os
+#!/usr/bin/python
 
+# Import the required modules
+import cv2, os
 import numpy as np
-
 from PIL import Image
-import cv2
 
-cascadePath = cv2.data.haarcascades + "haarcascade_frontalface_default.xml"
+# For face detection we will use the Haar Cascade provided by OpenCV.
+cascadePath = "haarcascade_frontalface_default.xml"
 faceCascade = cv2.CascadeClassifier(cascadePath)
-recognizer = cv2.face.LBPHFaceRecognizer_create()
 
-
-def image_to_label(image_path):
-    filename = os.path.split(image_path)[1]
-    no_extension = filename.split(".")[0]
-    subject_num = int(no_extension.replace("subject", ""))
-    return subject_num
-
+# For face recognition we will the the LBPH Face Recognizer 
+recognizer = cv2.createLBPHFaceRecognizer()
 
 def get_images_and_labels(path):
-    # Files with this extension will go into the testing set
-    testing_extension = ".sad"
-    image_paths = [os.path.join(path, file) for file in os.listdir(path) if not file.endswith(testing_extension)]
+    # Append all the absolute image paths in a list image_paths
+    # We will not read the image with the .sad extension in the training set
+    # Rather, we will use them to test our accuracy of the training
+    image_paths = [os.path.join(path, f) for f in os.listdir(path) if not f.endswith('.sad')]
     # images will contains face images
     images = []
     # labels will contains the label that is assigned to the image
     labels = []
     for image_path in image_paths:
         # Read the image and convert to grayscale
-        image_pil = Image.open(image_path).convert("L")
+        image_pil = Image.open(image_path).convert('L')
         # Convert the image format into numpy array
-        image = np.array(image_pil, "uint8")
+        image = np.array(image_pil, 'uint8')
         # Get the label of the image
-        nbr = image_to_label(image_path)
+        nbr = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
         # Detect the face in the image
         faces = faceCascade.detectMultiScale(image)
         # If face is detected, append the face to images and the label to labels
@@ -43,16 +39,14 @@ def get_images_and_labels(path):
     # return the images list and labels list
     return images, labels
 
-
 # Path to the Yale Dataset
-path = "yalefaces"
-# The folder yalefaces is in the same folder as this python script
-# Call the get_images_and_labels function and get the face images and the
+path = './yalefaces'
+# Call the get_images_and_labels function and get the face images and the 
 # corresponding labels
 images, labels = get_images_and_labels(path)
 cv2.destroyAllWindows()
 
-# Perform the training
+# Perform the tranining
 recognizer.train(images, np.array(labels))
 
 # Append the images with the extension .sad into image_paths
@@ -65,8 +59,8 @@ for image_path in image_paths:
         nbr_predicted, conf = recognizer.predict(predict_image[y: y + h, x: x + w])
         nbr_actual = int(os.path.split(image_path)[1].split(".")[0].replace("subject", ""))
         if nbr_actual == nbr_predicted:
-            print("{} is Correctly Recognized with confidence {}".format(nbr_actual, conf))
+            print "{} is Correctly Recognized with confidence {}".format(nbr_actual, conf)
         else:
-            print("{} is Incorrectly Recognized as {}".format(nbr_actual, nbr_predicted))
+            print "{} is Incorrect Recognized as {}".format(nbr_actual, nbr_predicted)
         cv2.imshow("Recognizing Face", predict_image[y: y + h, x: x + w])
         cv2.waitKey(1000)
